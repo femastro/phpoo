909b4e6f51d0c3d304d068b06495ce916cafdfd0
femastro <fernando.mastropietro@gmail.com>
2020-10-11 20:01:52 -0300

    First Commit

--- poo/index.php
+++ poo/index.php
@@ -8,7 +8,6 @@
     	$search= "%%";
     }
 
-
     $jugadores = new Jugadores();
 
     $jugadores= $jugadores->all($search); // carga los datos en la clase. y ejecuta la funcion enviando el dato de busqueda para el SQL y Recibe el array que devuelve la function all() de Jugadores.php
